997601e94dc8dc2a6e899afbb9b749db89ddad1c
Willemijn Beks <willemijn07@hotmail.com>
2015-01-13 14:57:39 +0100

    djdj

--- admin_addcategory.php
+++ admin_addcategory.php
@@ -1,8 +1,21 @@
 <?php
+require_once 'classes.php';
+if (isset($_POST['category']) {
+    $category = $_POST['add_category'];
+    ProductTypes::create($category);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include 'views/header.php';
+include 'views/navigation.php';
+?>
+	<p>
+		<br>
+		<a href="fruyt.nl/managerpanel" > <font color="#006666"> Managerpanel</font></a> / categorie toevoegen 			
+	</p>
+	<form action="admin_addcategory.php">					
+		<input type="text" name="add_category" placeholder="nieuwe categorie" id="add_category"> 
+		<input type="submit" value="opslaan" id="submit">		
+	</form>	
 
+<?php
+include 'views/footer.php';
+?>
