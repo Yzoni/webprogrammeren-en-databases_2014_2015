e2571bc35bb2eb30fc3d31651573043261b16d73
Willemijn Beks <willemijn07@hotmail.com>
2015-01-13 16:25:00 +0100

    huehei

--- admin_add_category.php
+++ admin_add_category.php
@@ -4,7 +4,7 @@ if (isset($_POST['add_category'])){
     $category = $_POST['add_category'];
     ProductType::create($category);
 }
-    
+
 include 'views/header.php';
 include 'views/navigation.php';
 ?>
@@ -12,7 +12,7 @@ include 'views/navigation.php';
 		<br>
 		<a href="fruyt.nl/managerpanel" > <font color="#006666"> Managerpanel</font></a> / categorie toevoegen 			
 	</p>
-	<form action="admin_add_category.php">					
+	<form action="admin_add_category.php" method="post">					
 		<input type="text" name="add_category" placeholder="nieuwe categorie" id="add_new_category"> 
 		<input type="submit" value="opslaan" id="submit_button">		
 	</form>	
