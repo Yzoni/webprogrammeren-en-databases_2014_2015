d6e95454523d5bffa87bd1097fe82b2625f5de6f
Willemijn Beks <willemijn07@hotmail.com>
2015-01-14 14:52:27 +0100

    meeoe

--- admin_add_category.php
+++ admin_add_category.php
@@ -12,7 +12,7 @@ include 'views/navigation.php';
 		<br>
 		<a href="fruyt.nl/managerpanel" > <font color="#006666"> Managerpanel</font></a> / categorie toevoegen 			
 	</p>
-	<form action="admin_add_category.php" method="post">					o
+	<form action="admin_add_category.php" method="post">					
 		<input type="text" name="add_category" placeholder="nieuwe categorie" id="add_new_category"> 
 		<input type="submit" value="opslaan" id="submit_button">		
 	</form>
