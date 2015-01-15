abcfc6d74784e46c03670901528ad42958b040e2
Willemijn Beks <willemijn07@hotmail.com>
2015-01-15 15:44:30 +0100

    rjlrjr

--- views/Product_displayBox.php
+++ views/Product_displayBox.php
@@ -11,5 +11,5 @@
     </p>
     <a href="view_product.php?id=<?php echo $this->id; ?>"><img class="image" src="data:image/png;base64,<?php echo base64_encode($this->image); ?>"/></a>
     <p class="stock"><span>&#xf00c;</span><?php echo $this->stock; ?> op voorraad</p>
-    <input type="button" class="button" value="<span>&#xf055;</span> | voeg toe" onclick=addToCart() />
+    <input type="button" class="button" value= " | voeg toe " onclick=addToCart() />
 </div>
