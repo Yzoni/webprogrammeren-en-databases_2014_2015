98c5cd71e47918ccd4b5498d3e8c47ca0e00ab30
Gideon Mooijen <gideonmooijen@wcw-stud-145-109-27-49.wireless.uva.nl>
2015-01-20 13:52:09 +0100

    Edited style button (add to cart on product page)

--- view_product.php
+++ view_product.php
@@ -90,7 +90,7 @@ $product = new Product($_GET["id"]);
                 if (is_admin_logged_in() == false) {
                     echo 'aantal: <input type="text" class="inputBox" '
                     . 'name="quantity">';
-                    echo '<input class="voegToe" type ="submit" '
+                    echo '<input class="submit" type ="submit" '
                     . 'value= "&#xf0fe;'
                     . ' | voeg toe">';
                 }
