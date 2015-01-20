a172d9b0f74d4dc45a6ddb9ce16835b045393aea
Gideon Mooijen <gideonmooijen@wcw-stud-145-109-27-49.wireless.uva.nl>
2015-01-20 15:00:06 +0100

    Style

--- customer_login.php
+++ customer_login.php
@@ -23,7 +23,7 @@ include 'views/navigation.php';
             <form action="customer_login.php" method="post">
                 <input type="text" name="email" placeholder="email"><br>
                 <input type="password" name="password" placeholder="wachtwoord"><br>
-                <input type="submit" value="inloggen" class="button">
+                <input type="submit" value="&#xf084; | inloggen" class="button">
             </form>
 	    <a href="customer_passwordforgot.php" class="button"><span>&#xf059;</span> | wachtwoord vergeten?</a>
         </div>
