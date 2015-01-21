65fa31c78f4ac594461d12b2c08d4653405848cd
Yorick de Boer <mail@yorickdeboer.nl>
2015-01-21 19:06:42 +0100

    klantenlijst

--- admin_list_customers.php
+++ admin_list_customers.php
@@ -3,17 +3,35 @@ require_once 'classes.php';
 security_check_admin();
 include 'views/header.php';
 include 'views/navigation.php';
+$customers = Customer::getAllCustomers();
 ?>
 
 <div class="wrappercontent">
     <h2 class="contenttitle">Mijn klanten</h2>
-    <?php
-    $customers = Customer::getAllCustomers();
-    foreach ($customers as $customer) {
-        $customer->displayBox();
-    }
-    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Voornaam</th>
+                <th>Achternaam</th>
+                <th>Email</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($customers as $customer) {
+                echo "<tr>";
+                echo "<td>" . $customer->firstname . "</std>";
+                echo "<td>" . $customer->lastname . "</td>";
+                echo "<td>" . $customer->email . "</td>";
+                echo "<td> <a href=\"admin_view_customer.php?id=" . $customer->id . "\" class=\"detailslink\">details ></a></td";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </div>
+
 <?php
 include 'views/footer.php';
 ?>
