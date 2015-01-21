ced84cf2ad37f3d33ec534d72e46fa61123a2e09
Yorick de Boer <mail@yorickdeboer.nl>
2015-01-21 23:29:23 +0100

    Laat gewoon ff geen plaatje zien als die er niet is daar worden we toch allemaal vrolijker van of niet soms

--- views/Product_displayBox.php
+++ views/Product_displayBox.php
@@ -9,8 +9,15 @@
         <a class="productpagelink" href="view_product.php?id=<?php echo $this->id; ?>"> <?php echo $this->name; ?></a>
     </p>
 
-    <a href="view_product.php?id=<?php echo$this->id; ?>"><img class="image" src="data:image/png;base64,<?php echo base64_encode($this->image); ?>"/></a>
+
+    <?php
+    if (is_null($this->image)) {
+        echo "<a href=\"view_product.php?id=" . $this->id . "\"><div class=\"image\"> </div></a>";
+    } else {
+        echo "<a href=\"view_product.php?id=" . $this->id . "\"><img class=\"image\" src = \"data:image/png;base64," . base64_encode($this->image) . "\"/></a>";
+    }
+    ?>
     <p class="stock"><span><?php echo ($this->stock > 0 ? "&#xf00c;" : "&#xf00d") ?></span><?php echo $this->stock; ?> op voorraad</p>
-            <?php echo "<p class=\"price\">&#8364;" . $this->price . "/kg</p>"; ?>
+    <?php echo "<p class=\"price\">&#8364;" . $this->price . "/kg</p>"; ?>
 
 </div>
