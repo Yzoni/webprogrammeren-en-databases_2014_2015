01c158cee3af092a1ef5b9f66555487e78f1c28e
Willemijn Beks <willemijn07@hotmail.com>
2015-01-21 14:25:27 +0100

    view product update

---
+++ functions.js
@@ -0,0 +1,10 @@
+function validQuantity(quantity, stock, cartQuantity) {
+	if (quantity + cartQuantity > stock || quantity <= 0) {	
+		writeln("nooooooooooooooooooooooooooo")	;
+		return false;
+	} else if {
+		return true;
+	}
+}
+
+
