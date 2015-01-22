e602441e5a177e662c3646c8beb8bbdd38af5da2
Gideon Mooijen <gideonmooijen@wcw-stud-145-109-27-49.wireless.uva.nl>
2015-01-22 12:11:43 +0100

    Edited view_product page

--- view_product.php
+++ view_product.php
@@ -84,6 +84,8 @@ if (isset($_POST['quantity'])) {
 include 'views/header.php';
 include 'views/navigation.php';
 ?>
+<div class="wrappercontent">
+
     <div>
         <br>
         <a href="products.php?id=<?php echo $product->type->id ?>
@@ -175,6 +177,7 @@ include 'views/navigation.php';
            class="button"><span>&#xf137;</span>terug naar: 
             <?php echo $product->type->name; ?> </a>
     </p>
+</div>
 
 <?php
 include 'views/footer.php';
