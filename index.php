9d999421cda798225727928d42dcca05848346c7
Yorick de Boer <mail@yorickdeboer.nl>
2015-01-22 22:42:14 +0100

    uitgelicht

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include 'views/navigation.php';
 ?>
 
 <div class = "wrappercontent">
+    <i><h2>Uitgelichte producten...</h2></i>
     <?php
     $special = 1;
     $products = Product::getAllProducts(null, 0, 5, $special);
