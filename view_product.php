a837fea36352e3458c757ffed6e3eb3b3b611a3e
Gideon Mooijen <gideonmooijen@wcw-stud-145-109-27-49.wireless.uva.nl>
2015-01-23 11:51:18 +0100

    Edited view_product

--- view_product.php
+++ view_product.php
@@ -134,14 +134,14 @@ function validQuantity() {
     </li>
     
     <li>
-        <span class="prodInfoTxt"> 
+        <p class="prodInfoTxt"> 
         <span class="icon">&#xf135; </span>
             levertijd: 1 dag
         </span>
         <br>
     </li>
     <li>
-        <span class="prodInfoTxt">        
+        <p class="prodInfoTxt">        
         <span class="icon">&#xf153; </span> 
             prijs per kg: 
                 <?php echo $product->price; ?> euro
