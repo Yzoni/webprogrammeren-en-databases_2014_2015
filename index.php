b6ed268e6bf1c0ffb05104a923f344c3c403f623
Yorick de Boer <mail@yorickdeboer.nl>
2015-01-23 18:13:07 +0100

    stuff

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 require_once 'classes.php';
 include 'views/header.php';
 include 'views/navigation.php';
-if(extension_loaded('imagick')) {
-    echo 'Imagick Loaded';
-} else {
-    echo 'NO Loaded';
-}
     
 ?>
 
