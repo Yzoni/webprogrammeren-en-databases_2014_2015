c12dafcf840ac9ac259cf2204ce833968593f72f
Yorick de Boer <mail@yorickdeboer.nl>
2015-01-25 13:18:31 +0100

    maak query in search zichtbaar

--- searchdropdown.php
+++ searchdropdown.php
@@ -6,7 +6,7 @@ $hint = "";
 if (strlen($q) > 1) {
     $products = Product::search($q);
     foreach ($products as $product) {
-        $hint .= "<a href=\"view_product.php?id=" . $product->id . "\">" . $product->name . "  (" . $product->type->name . ")</a>";
+        $hint .= "<a href=\"view_product.php?id=" . $product->id . "\">" . replaceWithBold($product->name, $q) . "  (" . $product->type->name . ")</a>";
     }
 }
 
@@ -17,4 +17,16 @@ if ($hint == "") {
 }
 
 echo $response;
+
+function replaceWithBold($product, $search) {
+    $startpossearch = strpos($product, $search);
+    $endpossearch = strlen($search) + $startpossearch;
+    
+    $beforesearch = substr($product, 0, $startpossearch);
+    $aftersearch = substr($product, $endpossearch);
+    
+    $boldsearch = "<span class=\"queryinword\">" . $search . "</span>"; 
+    
+    return $beforesearch . $boldsearch . $aftersearch;  
+}
 ?>
