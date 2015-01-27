d70fbcefcaaa97d500f86cfd501ed2f132231d8b
Gideon Mooijen <gideonmooijen@wcw-stud-145-109-27-49.wireless.uva.nl>
2015-01-27 15:17:00 +0100

    Fixed posit

--- view_product.php
+++ view_product.php
@@ -133,6 +133,9 @@ if (!empty($_SESSION['products'])) {
     </div>
     <div class="hrline"></div>
     <div class="underdescription">
+	  <a id="backtocategory" href="products.php?id=<?php echo $product->type->id ?>" 
+               class="button"><span>&#xf137;</span>terug naar: 
+                <?php echo $product->type->name; ?> </a>
 	<table class="product_info">
 	  <tr>
 	    <td width="20">
@@ -176,9 +179,6 @@ if (!empty($_SESSION['products'])) {
 
         </div>
 	</div>          
-	  <a id="backtocategory" href="products.php?id=<?php echo $product->type->id ?>" 
-               class="button"><span>&#xf137;</span>terug naar: 
-                <?php echo $product->type->name; ?> </a>
 </div>
 
 <?php
