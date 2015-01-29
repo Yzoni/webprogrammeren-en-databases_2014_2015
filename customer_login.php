4040548926cf7d829641e5923d8d07ade45e516c
Julian Main <main.jx@gmail.com>
2015-01-29 10:37:36 +0100

    edit

--- customer_login.php
+++ customer_login.php
@@ -30,7 +30,7 @@ include 'views/navigation.php';
 <?php
 
 if(isset($_SESSION['loginFalse']) && $_SESSION['loginFalse'] = 1) {
-    echo "U moet ingelogd zijn om af te rekenen. Indien u geen account heeft"
+    echo "U moet ingelogd zijn om af te rekenen. Indien u geen account heeft "
     . "klikt u dan <a href='customer_register.php'> hier </a> om te registreren.";
 }
 ?>
