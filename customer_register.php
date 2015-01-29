a7d64e4f2d84ef20f123fcd995c363cc13f0d918
Yzoni <yzonix@gmail.com>
2015-01-29 16:26:33 +0100

    verwijder form stuff

--- customer_register.php
+++ customer_register.php
@@ -31,25 +31,17 @@ include 'views/navigation.php';
     <div class="wrapperregister">
     <form action="customer_register.php" method="POST">
         <input id="email" type="text" name="email" placeholder="emailadres" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"  onkeyup="checkForm()"><br>
-        <span id="error0" >Veld mag niet leeg zijn!</span><br>
         <input type="password" name="password" placeholder="wachtwoord" id="password1" onkeyup="checkForm()">
-        <span id="error1" >Veld mag niet leeg zijn!</span><br>
         <input type="password" name="password2" placeholder="wachtwoord (nogmaals)" id="password2" onkeyup="checkForm()">
-        <span id="error2" >Veld mag niet leeg zijn!</span><br>
         <select name="gender" class="select_gender">
             <option value="true">Dhr.</option>
             <option value="false">Mevr.</option>
         </select>
         <input id="firstname" type="text" name="firstname" placeholder="voornaam" onkeyup="checkForm()"><br>
-        <span id="error3" >Veld mag niet leeg zijn!</span><br>
         <input id="lastname" type="text" name="lastname" placeholder="achternaam" onkeyup="checkForm()"><br>
-        <span id="error4" >Veld mag niet leeg zijn!</span><br>
         <input id="zip" type="text" name="zip" placeholder="postcode" pattern="[0-9]{4}[a-zA-Z]{2}$" onkeyup="checkForm()"><br>
-        <span id="error5" >Veld mag niet leeg zijn!</span><br>
         <input id="streetname" type="text" name="streetaddress" placeholder="straatnaam" onkeyup="checkForm()"><br>
-        <span id="error6" >Veld mag niet leeg zijn!</span><br>
         <input id="streetnumber" type="text" name="streetnumber" placeholder="huisnummer" onkeyup="checkForm()"><br>
-        <span id="error7" >Veld mag niet leeg zijn!</span><br>
         <button id="submitbutton" class="button" type="submit"><span>&#xf14a;</span>registreer</button>
     </form>
     </div>
