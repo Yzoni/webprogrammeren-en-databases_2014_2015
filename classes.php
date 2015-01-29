f0b77eb10f671761e38ce39bf077425098f278b5
Gideon Mooijen <gideonmooijen@wcw-stud-145-109-27-49.wireless.uva.nl>
2015-01-29 10:42:10 +0100

    Minor edits

--- classes.php
+++ classes.php
@@ -704,7 +704,7 @@ class Admin {
             $i += 1;
         }
         echo "<table>";
-        echo "<th> order nummer</th>";
+        echo "<th>Ordernummer</th>";
         echo "<th>Klantnummer</th>";
         echo "<th>Datum</th>";
         foreach ($_SESSION['orders'] as $order) {
@@ -725,7 +725,7 @@ class Admin {
     static function show_order($orderID) {
         global $db;
         $date = Order::show_date($orderID);
-        echo "<table>";
+        echo "<table class='order'>";
         echo "<tr>";
         echo "<td> factuurnummer: $orderID <br> $date</td>";
         echo "</tr>";
@@ -896,7 +896,7 @@ class Order {
         $date = Order::show_date($orderID);
         echo "<table class='order'>";
         echo "<tr>";
-        echo "<td> factuurnummer: $orderID </td>";
+        echo "<td> Factuurnummer: $orderID </td>";
 	echo "<td>$date</td>";
         echo "</tr>";
         Order::show_company_Info();
@@ -907,7 +907,7 @@ class Order {
 
     static function show_company_Info() {
         echo "<td class='company_info'>";
-        echo "<h3>BedrijfsInformatie</h3> <br>";
+        echo "<h3>Bedrijfsinformatie</h3> <br>";
         echo "Adres: Fruytlaan 904 1234AB <br>";
         echo "Tel: 0201235813 <br>";
         echo "KvK: 12345678<br>";
